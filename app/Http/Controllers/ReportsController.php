c32c7f40821d01bc17a8197da4d852ab1092b959
Abdrashov <abdrashovv@gmail.com>
2022-03-04 01:57:02 +0600

    updated sql query

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -11,71 +11,39 @@ class ReportsController extends Controller
 {
     public function index()
     {
-        // $reports = Organization::filter(Request::only('search', 'trashed'))
-        //     ->select(
-        //         'organizations.id',
-        //         'organizations.name',
-        //         'organizations.created_at',
-        //         DB::raw(
-        //             '(SELECT COUNT(invoices.status) FROM `invoices` WHERE `organizations`.`id` = `invoices`.`organization_id` AND `invoices`.`status` = 1) as confirmed'
-        //         ),
-        //         DB::raw(
-        //             '(SELECT COUNT(invoices.status) FROM `invoices` WHERE `organizations`.`id` = `invoices`.`organization_id` AND `invoices`.`status` = 0) as not_confirmed'
-        //         ),
-        //         DB::raw(
-        //             'SUM(invoice_items.price * invoice_items.count) as sum_price'
-        //         ),
-        //         DB::raw(
-        //             'SUM(invoice_items.count) as sum_count'
-        //         )
-        //     )
-        //     ->leftJoin('invoices', 'organizations.id', '=', 'invoices.organization_id')
-        //     ->leftJoin('invoice_items', 'invoices.id', '=', 'invoice_items.invoice_id')
-        //     ->groupBy('organizations.id')
-        //     ->paginate(20)
-        //     ->withQueryString()
-        //     ->through(fn ($report) => [
-        //         'id' => $report->id,
-        //         'name' => $report->name,
-        //         'created_at' => $report->created_at->format('Y-m-d'),
-        //         'confirmed' => $report->confirmed ?? 0,
-        //         'not_confirmed' => $report->not_confirmed ?? 0,
-        //         'sum_price' => $report->sum_price ?? 0,
-        //         'sum_count' => $report->sum_count ?? 0,
-        //     ]);
-            $reports = Organization::filter(Request::only('search', 'trashed'))
-                ->select(
-                    'organizations.id',
-                    'organizations.name',
-                    'organizations.created_at',
-                    DB::raw(
-                        '(SELECT COUNT(invoices.status) FROM `invoices` WHERE `organizations`.`id` = `invoices`.`organization_id` AND `invoices`.`status` = 1) as confirmed'
-                    ),
-                    DB::raw(
-                        '(SELECT COUNT(invoices.status) FROM `invoices` WHERE `organizations`.`id` = `invoices`.`organization_id` AND `invoices`.`status` = 0) as not_confirmed'
-                    ),
-                    DB::raw(
-                        'SUM(invoice_items.price * invoice_items.count) as sum_price'
-                    ),
-                    DB::raw(
-                        'SUM(invoice_items.count) as sum_count'
-                    )
+        $reports = Organization::filter(Request::only('search', 'trashed'))
+            ->select(
+                'organizations.id',
+                'organizations.name',
+                'organizations.created_at',
+                DB::raw(
+                    '(SELECT COUNT(invoices.status) FROM `invoices` WHERE `organizations`.`id` = `invoices`.`organization_id` AND `invoices`.`status` = 1) as confirmed'
+                ),
+                DB::raw(
+                    '(SELECT COUNT(invoices.status) FROM `invoices` WHERE `organizations`.`id` = `invoices`.`organization_id` AND `invoices`.`status` = 0) as not_confirmed'
+                ),
+                DB::raw(
+                    'SUM(invoice_items.price * invoice_items.count) as sum_price'
+                ),
+                DB::raw(
+                    'SUM(invoice_items.count) as sum_count'
                 )
-                ->leftJoin('invoices', 'organizations.id', '=', 'invoices.organization_id')
-                ->leftJoin('invoice_items', 'invoices.id', '=', 'invoice_items.invoice_id')
-                ->groupBy('organizations.id')
-                ->paginate(20)
-                ->withQueryString()
-                ->through(fn ($report) => [
-                    'id' => $report->id,
-                    'name' => $report->name,
-                    'created_at' => $report->created_at->format('Y-m-d'),
-                    'confirmed' => $report->invoices()->where('status', 1)->count(),
-                    'not_confirmed' => $report->invoices()->where('status', 0)->count(),
-                    'sum_price' => $report->sum_price ?? 0,
-                    'sum_count' => $report->sum_count ?? 0,
-                ]);
-            
+            )
+            ->leftJoin('invoices', 'organizations.id', '=', 'invoices.organization_id')
+            ->leftJoin('invoice_items', 'invoices.id', '=', 'invoice_items.invoice_id')
+            ->groupBy('organizations.id')
+            ->paginate(20)
+            ->withQueryString()
+            ->through(fn ($report) => [
+                'id' => $report->id,
+                'name' => $report->name,
+                'created_at' => $report->created_at->format('Y-m-d'),
+                'confirmed' => $report->confirmed ?? 0,
+                'not_confirmed' => $report->not_confirmed ?? 0,
+                'sum_price' => $report->sum_price ?? 0,
+                'sum_count' => $report->sum_count ?? 0,
+            ]);
+
         return Inertia::render('Reports/Index', [
             'filters' => Request::all('search', 'trashed'),
             'organizations' => $reports,
