b67450add5c5b9257d8dbd55857241f95e7f38ca
abdrashov <abdrashovv@gmail.com>
2022-03-05 12:15:09 +0600

    server

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Invoice;
+use App\Models\Item;
+use App\Models\Organization;
+use App\Models\User;
 use Inertia\Inertia;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return Inertia::render('Dashboard/Index');
+        $users_count = User::count();
+        $organizations_count = Organization::count();
+        $items_count = Item::count();
+        $invoices_count = Invoice::count();
+
+        return Inertia::render('Dashboard/Index', [
+            'users_count' => $users_count,
+            'organizations_count' => $organizations_count,
+            'items_count' => $items_count,
+            'invoices_count' => $invoices_count,
+        ]);
     }
 }
--- resources/js/Pages/Dashboard/Index.vue
+++ resources/js/Pages/Dashboard/Index.vue
@@ -1,9 +1,70 @@
 <template>
-  <div>
-    <Head title="Панель управления" />
-    <h1 class="mb-6 text-2xl font-semibold">Панель управления</h1>
+    <div>
+        <Head title="Панель управления" />
+        <h1 class="mb-6 text-2xl font-semibold">Панель управления</h1>
 
-  </div>
+        <div class="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
+            <!-- Card -->
+            <div class="flex items-center p-4 bg-white rounded-lg shadow-xs">
+                <div class="p-3 mr-4 text-orange-500 bg-orange-100 rounded-full">
+                    <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+                        <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z"></path>
+                    </svg>
+                </div>
+                <div>
+                    <p class="text-sm font-medium text-gray-600">Пользователи</p>
+                    <p class="text-lg font-semibold text-gray-700">{{ users_count }}</p>
+                </div>
+            </div>
+            <!-- Card -->
+            <div class="flex items-center p-4 bg-white rounded-lg shadow-xs">
+                <div class="p-3 mr-4 text-green-500 bg-green-100 rounded-full">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" viewBox="0 0 24 24" fill="currentColor">
+                        <path opacity="0.3" d="M5 8.04999L11.8 11.95V19.85L5 15.85V8.04999Z" />
+                        <path
+                            d="M20.1 6.65L12.3 2.15C12 1.95 11.6 1.95 11.3 2.15L3.5 6.65C3.2 6.85 3 7.15 3 7.45V16.45C3 16.75 3.2 17.15 3.5 17.25L11.3 21.75C11.5 21.85 11.6 21.85 11.8 21.85C12 21.85 12.1 21.85 12.3 21.75L20.1 17.25C20.4 17.05 20.6 16.75 20.6 16.45V7.45C20.6 7.15 20.4 6.75 20.1 6.65ZM5 15.85V7.95L11.8 4.05L18.6 7.95L11.8 11.95V19.85L5 15.85Z"
+                        />
+                    </svg>
+                </div>
+                <div>
+                    <p class="text-sm font-medium text-gray-600">Объекты</p>
+                    <p class="text-lg font-semibold text-gray-700">{{ organizations_count }}</p>
+                </div>
+            </div>
+            <!-- Card -->
+            <div class="flex items-center p-4 bg-white rounded-lg shadow-xs">
+                <div class="p-3 mr-4 text-blue-500 bg-blue-100 rounded-full">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
+                        <path
+                            opacity="0.4"
+                            d="M19 22H5C4.4 22 4 21.6 4 21V3C4 2.4 4.4 2 5 2H14L20 8V21C20 21.6 19.6 22 19 22ZM15 17C15 16.4 14.6 16 14 16H8C7.4 16 7 16.4 7 17C7 17.6 7.4 18 8 18H14C14.6 18 15 17.6 15 17ZM17 12C17 11.4 16.6 11 16 11H8C7.4 11 7 11.4 7 12C7 12.6 7.4 13 8 13H16C16.6 13 17 12.6 17 12ZM17 7C17 6.4 16.6 6 16 6H8C7.4 6 7 6.4 7 7C7 7.6 7.4 8 8 8H16C16.6 8 17 7.6 17 7Z"
+                        />
+                        <path d="M15 8H20L14 2V7C14 7.6 14.4 8 15 8Z" />
+                    </svg>
+                </div>
+                <div>
+                    <p class="text-sm font-medium text-gray-600">Накладные</p>
+                    <p class="text-lg font-semibold text-gray-700">{{ invoices_count }}</p>
+                </div>
+            </div>
+            <!-- Card -->
+            <div class="flex items-center p-4 bg-white rounded-lg shadow-xs">
+                <div class="p-3 mr-4 rounded-full text-amber-500 bg-amber-100">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-5 h-5" viewBox="0 0 24 24">
+                        <path
+                            opacity="0.3"
+                            d="M21.25 18.525L13.05 21.825C12.35 22.125 11.65 22.125 10.95 21.825L2.75 18.525C1.75 18.125 1.75 16.725 2.75 16.325L4.04999 15.825L10.25 18.325C10.85 18.525 11.45 18.625 12.05 18.625C12.65 18.625 13.25 18.525 13.85 18.325L20.05 15.825L21.35 16.325C22.35 16.725 22.35 18.125 21.25 18.525ZM13.05 16.425L21.25 13.125C22.25 12.725 22.25 11.325 21.25 10.925L13.05 7.62502C12.35 7.32502 11.65 7.32502 10.95 7.62502L2.75 10.925C1.75 11.325 1.75 12.725 2.75 13.125L10.95 16.425C11.65 16.725 12.45 16.725 13.05 16.425Z"
+                        />
+                        <path d="M11.05 11.025L2.84998 7.725C1.84998 7.325 1.84998 5.925 2.84998 5.525L11.05 2.225C11.75 1.925 12.45 1.925 13.15 2.225L21.35 5.525C22.35 5.925 22.35 7.325 21.35 7.725L13.05 11.025C12.45 11.325 11.65 11.325 11.05 11.025Z" />
+                    </svg>
+                </div>
+                <div>
+                    <p class="text-sm font-medium text-gray-600">Товары</p>
+                    <p class="text-lg font-semibold text-gray-700">{{ items_count }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -11,9 +72,15 @@ import { Head } from '@inertiajs/inertia-vue3'
 import Layout from '@/Shared/Layout'
 
 export default {
-  components: {
-    Head,
-  },
-  layout: Layout,
+    components: {
+        Head,
+    },
+    layout: Layout,
+    props: {
+        users_count: Number,
+        organizations_count: Number,
+        items_count: Number,
+        invoices_count: Number,
+    },
 }
 </script>
