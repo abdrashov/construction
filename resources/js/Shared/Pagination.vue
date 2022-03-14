549fd0b84741c8e9c81f5da6a177e09a93c8bf58
abdrashov <abdrashovv@gmail.com>
2022-03-14 15:25:40 +0600

    pagination bg color form bg-white to bg-blue-400

--- resources/js/Shared/Pagination.vue
+++ resources/js/Shared/Pagination.vue
@@ -2,8 +2,8 @@
   <div v-if="links.length > 3">
     <div class="flex flex-wrap -mb-1 ">
       <template v-for="(link, key) in links">
-        <div v-if="link.url === null" :key="key" class="px-4 py-3 mb-1 mr-1 text-sm leading-4 text-gray-500 border rounded" v-html="link.label" />
-        <Link v-else :key="`link-${key}`" class="px-4 py-3 mb-1 mr-1 text-sm leading-4 bg-white border rounded focus:text-gray-500 hover:bg-gray-50 focus:border-gray-50"  :class="{ 'bg-gray-50': link.active }" :href="link.url" v-html="link.label" />
+        <div v-if="link.url === null" :key="key" class="px-4 py-3 mb-1 mr-1 text-sm leading-4 bg-white border rounded hover:text-white focus:text-blue-400 hover:bg-blue-400 focus:border-blue-400" v-html="link.label" :class="{ 'bg-blue-400 text-white': link.active }" />
+        <Link v-else :key="`link-${key}`" class="px-4 py-3 mb-1 mr-1 text-sm leading-4 bg-white border rounded hover:text-white focus:text-blue-400 hover:bg-blue-400 focus:border-blue-400" :class="{ 'bg-blue-400 text-white': link.active }" :href="link.url" v-html="link.label" />
       </template>
     </div>
   </div>
